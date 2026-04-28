f38986691bf44644b33a98cc06f9f8d24751607c
Rachit Gangwar <156359363+Rachit-0001@users.noreply.github.com>
2026-04-29 02:55:51 +0530

    Listing Models

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
+const Listing =require(".../models/listing.js");
 
+const Mongo_URL="mongodb://127.0.0.1:27017/wanderlust";
 app.get("/",(req,res)=>{
     res.send("Hi");
 });
--- models/listing.js
+++ models/listing.js
@@ -2,9 +2,15 @@ const moongose =require("mongoose");
 const Schema =moongose.Schema;
 
 const  listingSchema =new Schema({
-    title:String,
+    title:{
+        type: String,
+        required: true,
+    },
     description:String,
-    image:String,
+    image:{
+        type: String,
+        set: (v) => v === "" ? "default link" : v,
+    },
     price:Number,
     location:String,
     country:String
